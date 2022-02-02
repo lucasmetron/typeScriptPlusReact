4e75b798e5f2ba3648927cfb72c75e71dab93909
lucasmetron <lucasmetron@gmail.com>
2022-02-02 11:28:39 -0300

    primeiro exercicio

---
+++ 1Intro/index.ts
@@ -0,0 +1,13 @@
+const firtsName = "Lucas"
+const anotherName = 1
+const x = true
+
+
+function calc(num1:number, num2:number){
+    console.log(`Resultado ${ num1+num2}`)
+    return num1 + num2
+}
+
+calc(5, 10)
+// greeting(anotherName)
+// greeting(x)
